8e436c84966f4372f50de0d26dc37812a75776ab
Howard Wang <howardwang00@gmail.com>
2018-05-11 13:17:28 -0700

    Added Files to Repository

---
+++ google-maps/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import MapContainer from './MapContainer'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<MapContainer />, document.getElementById('root'));
+registerServiceWorker();
