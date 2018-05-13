6a834369404c1201625b364d54f9b82158481a3a
Howard Wang <howardwang00@gmail.com>
2018-05-13 16:44:20 -0700

    Added Google Maps from google-map-react Teacher Tutorial

--- group-vision-web/src/index.js
+++ group-vision-web/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MapContainer from './MapContainer'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MapContainer />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
